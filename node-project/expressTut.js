46f705fbe1633760d8ffac6eacb40fa8b3cfd086
shahidafridiweb <148265268+shahidafridiweb@users.noreply.github.com>
2024-03-21 14:27:20 +0530

    Rendering Static HTML pages

--- node-project/expressTut.js
+++ node-project/expressTut.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app=express();
 
 app.get("",(req,res)=>{
-    console.warn(req.query.name);
-    res.send("It is what it is "+ req.query.name)
-}).listen(3000)
+    console.warn(req.query.name?req.query.name:"Shahid");
+    res.send(`<h1>It is what it is ${req.query.name?req.query.name:"Shahid" }</h1>
+    <button><a href='/about'>Go to About </a></button>`)
+});
+
+app.get("/about",(req,res)=>{
+    res.send(`<h1>This is about Page</h1>
+    <button><a href='/'>Go to Home </a></button>`)
+});
+
+app.listen(3000);
